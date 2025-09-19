52d5f995a7a14dc0700901bbedf43f19116b39a3
Meliza Aguilar <johana.aguilar64@unach.mx>
2025-09-18 23:00:49 -0600

    Initial Commit

---
+++ SwiftDay/SwiftDayApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDayApp.swift
+//  SwiftDay
+//
+//  Created by Jesus Ortega on 18/09/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
